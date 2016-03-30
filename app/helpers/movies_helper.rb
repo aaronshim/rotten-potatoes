437aa168684a82ae251544e3226704245f316643
Aaron Shim <aaron.k.shim@gmail.com>
2016-03-30 01:38:55 -0400

    comments

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,6 +4,7 @@ module MoviesHelper
     count.odd? ?  "odd" :  "even"
   end
 
+  # resets column sorting on view when columns changed
   def prev_col_reset(column)
     @last_column == column ? @next_order : true
   end
